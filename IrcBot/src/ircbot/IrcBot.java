b944f14c25a15b747d8c1db800cde3591ab7620b
Yakuchi <yakuchi@paivola.fi>
2013-03-12 18:39:19 +0200

    nettipapu proju lisatty

---
+++ IrcBot/src/ircbot/IrcBot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ircbot;
+
+/**
+ *
+ * @author Miika
+ */
+public class IrcBot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
